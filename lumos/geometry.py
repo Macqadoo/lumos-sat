87885d84f07eb4cf58c48f630a2e5b48a29c59b6
Macqadoo <jasper.hudson@students.mq.edu.au>
2026-04-02 15:29:11 +1100

    Refactor geometry.py for improved readability and structure

--- lumos/geometry.py
+++ lumos/geometry.py
@@ -5,19 +5,24 @@ Geometry objects which are used throughout Lumos
 import numpy as np
 import lumos.constants
 import lumos.calculator
+import os
+import pandas as pd
+
+
 class Surface:
     """
-    Container to hold area, normal vector, and BRDF of a surface 
+    Container to hold area, normal vector, and BRDF of a surface
 
     :param area: Area of surface :math:`m^2`
     :type area: float
     :param normal: Normal vector of surface. Measured in brightness frame. A function of
-        the angle past terminator may be passed, which must return the surface normal as 
+        the angle past terminator may be passed, which must return the surface normal as
         a :class:`np.ndarray` for given angles past the terminator.
     :type normal: :class:`np.ndarray` or function
     :param brdf: Bidirectional Reflectance Distribution Function (BRDF) of surface.
     :type brdf: function
     """
+
     def __init__(self, area, normal, brdf):
         """
         Constructor Method
@@ -25,15 +30,18 @@ class Surface:
         self.area = area
         self.normal = normal
         self.brdf = brdf
-    
+
     def __str__(self):
         """
         Printing Method
         """
-        return f'| Surface \n' \
-               f'|-- Area: {self.area:.2f} m^2 \n' \
-               f'|-- Normal Vector: ' \
-               f'<{self.normal[0]:.2f}, {self.normal[1]:.2f}, {self.normal[2]:.2f}> \n'
+        return (
+            f"| Surface \n"
+            f"|-- Area: {self.area:.2f} m^2 \n"
+            f"|-- Normal Vector: "
+            f"<{self.normal[0]:.2f}, {self.normal[1]:.2f}, {self.normal[2]:.2f}> \n"
+        )
+
 
 class EarthMesh:
     """
@@ -44,34 +52,37 @@ class EarthMesh:
     :param angles_on_plane: 1D array of the angles-on-plane of the mesh.
     :type angles_on_plane: :class:`np.ndarray`
     """
+
     def __init__(self, angles_off_plane, angles_on_plane):
         """
         Constructor method
         """
         self.d_phi = np.abs(angles_off_plane[1] - angles_off_plane[0])
         self.d_theta = np.abs(angles_on_plane[1] - angles_on_plane[0])
-        
-        self.angles_off_plane, self.angles_on_plane = np.meshgrid(angles_off_plane,
-                                                                  angles_on_plane)
-        
+
+        self.angles_off_plane, self.angles_on_plane = np.meshgrid(
+            angles_off_plane, angles_on_plane
+        )
+
         self.dists_off_plane = lumos.constants.EARTH_RADIUS * self.angles_off_plane
         self.dists_on_plane = lumos.constants.EARTH_RADIUS * self.angles_on_plane
-        
+
         # Set up panel positions
-        self.z = ( lumos.constants.EARTH_RADIUS 
-                  / np.sqrt(1 + np.tan(self.angles_on_plane)**2 
-                            + np.tan(self.angles_off_plane)**2 ) )
-                  
+        self.z = lumos.constants.EARTH_RADIUS / np.sqrt(
+            1 + np.tan(self.angles_on_plane) ** 2 + np.tan(self.angles_off_plane) ** 2
+        )
+
         self.x = np.tan(self.angles_off_plane) * self.z
         self.y = np.tan(self.angles_on_plane) * self.z
-        
+
         # Get normal vectors of panels
         self.nx = self.x / lumos.constants.EARTH_RADIUS
         self.ny = self.y / lumos.constants.EARTH_RADIUS
         self.nz = self.z / lumos.constants.EARTH_RADIUS
 
         self.shape = self.z.shape
-        
+
+
 class GroundObservers(EarthMesh):
     """
     A mesh of observers visible to the satellite and on the night side of earth
@@ -88,23 +99,29 @@ class GroundObservers(EarthMesh):
         """
         Constructor Method
         """
-        self.max_angle = np.arccos(lumos.constants.EARTH_RADIUS 
-                                   / (lumos.constants.EARTH_RADIUS + sat_height))
+        self.max_angle = np.arccos(
+            lumos.constants.EARTH_RADIUS / (lumos.constants.EARTH_RADIUS + sat_height)
+        )
         self.min_angle = angle_past_terminator
-        
+
         angles_off_plane = np.linspace(-self.max_angle, self.max_angle, density)
         angles_on_plane = np.linspace(-self.max_angle, self.min_angle, density)
-        
+
         super().__init__(angles_off_plane, angles_on_plane)
         self.sat_height = sat_height
         self.angle_past_terminator = angle_past_terminator
-    
-    def calculate_intensity(self, surfaces,
-                            include_sun = True, include_earthshine = False, 
-                            earth_panel_density = 151, earth_brdf = None):
+
+    def calculate_intensity(
+        self,
+        surfaces,
+        include_sun=True,
+        include_earthshine=False,
+        earth_panel_density=151,
+        earth_brdf=None,
+    ):
         """
         Calculates intensity for observers on ground
-        
+
         :param surfaces: List of satellite surfaces
         :type surfaces: List[:py:func:`lumos.geometry.Surface`]
         :param include_sun: Whether to include contribution of brightness due to direct sunlight
@@ -122,14 +139,14 @@ class GroundObservers(EarthMesh):
 
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
-                
-                self.intensities[i, j] = \
-                    lumos.calculator.get_intensity_satellite_frame(
-                        surfaces,
-                        self.sat_height,
-                        self.angle_past_terminator,
-                        (self.x[i, j], self.y[i, j], self.z[i, j]),
-                        include_sun,
-                        include_earthshine,
-                        earth_panel_density,
-                        earth_brdf)
+
+                self.intensities[i, j] = lumos.calculator.get_intensity_satellite_frame(
+                    surfaces,
+                    self.sat_height,
+                    self.angle_past_terminator,
+                    (self.x[i, j], self.y[i, j], self.z[i, j]),
+                    include_sun,
+                    include_earthshine,
+                    earth_panel_density,
+                    earth_brdf,
+                )
